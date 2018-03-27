29527ca09bf08085ebe627056536af275c125a3a
maxoon <desfouxmaxime@gmail.com>
2018-03-27 14:38:51 +0200

    add : businessException referentielDAOimpl

--- src/tpQCM/dal/ReferentielDAO.java
+++ src/tpQCM/dal/ReferentielDAO.java
@@ -1,11 +1,12 @@
 package tpQCM.dal;
 
+import tpQCM.BusinessException;
 import tpQCM.bo.Question;
 
 public interface ReferentielDAO {
 
-	public Question addQuestion(Question q);
+	public Question addQuestion(Question q) throws BusinessException;
 	
-	public int removeQuestion(int id);
+	public int removeQuestion(int id) throws BusinessException;
 	
 }
