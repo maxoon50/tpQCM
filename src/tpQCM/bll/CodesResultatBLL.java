ac444a9529c40af41d3b32dba7f14de88290f6bc
maxoon <desfouxmaxime@gmail.com>
2018-03-28 09:51:07 +0200

    add : exceptions to ReferentielManager

--- src/tpQCM/bll/CodesResultatBLL.java
+++ src/tpQCM/bll/CodesResultatBLL.java
@@ -1,7 +1,16 @@
 package tpQCM.bll;
 
+import tpQCM.bo.Proposition;
+
 public abstract class CodesResultatBLL {
 	
+	///////////////REGLES GENERIQUES///////////////////////////////////////////////////////////
+	/**
+	 *  régle pour objet passé null
+	 */
+	public static final int OBJET_NULL = 21000;
+	
+	///////////////REGLES USER///////////////////////////////////////////////////////////
 	/**
 	 * Echec le nom de l'article ne respecte pas les règles définies
 	 */
@@ -20,4 +29,40 @@ public abstract class CodesResultatBLL {
 	public static final int ERREUR_EMAIL_INCONNU=20005;
 	
 	public static final int ERREUR_LOGIN=20006;
+	
+	///////////////REGLES REFERENTIEL///////////////////////////////////////////////////////////
+	
+	/**
+	 * Echec longueur énoncé <10
+	 */
+	public static final int REGLE_REFERENTIEL_ENONCE_ERREUR=20100;
+	
+	/**
+	 * Echec il y a moins de 4 propositions à la question
+	 */
+	public static final int REGLE_REFERENTIEL_LISTPROP_LONG_ERREUR=20101;
+	
+
+	/**
+	 * Echec le point est inf à 0 ou sup à 10 ou null
+	 */
+	public static final int REGLE_REFERENTIEL_POINTS_ERREUR=20102;
+
+	/**
+	 * Echec l'élément uneReponse est  null
+	 */
+	public static final int REGLE_REFERENTIEL_UNEREPONSE_ERREUR=20103;
+	
+	/**
+	 * Echec la liste des propositions  est  null
+	 */
+	public static final int REGLE_REFERENTIEL_LISTPROP_ERREUR=20104;
+	
+	/**
+	 * Echec la proposition est nulle ou vide
+	 */
+	public static final int REGLE_REFERENTIEL_PROP_LONG_ERREUR=20105;
+	
+
+
 }
--- src/tpQCM/bll/ReferentielManager.java
+++ src/tpQCM/bll/ReferentielManager.java
@@ -14,9 +14,11 @@ public class ReferentielManager {
 		 dao = new ReferentielDAOJdbcImpl();
 	}
 	
-	public Question addQuestion(Question quest) {
+	public Question addQuestion(Question quest) throws BusinessException {
+		
 		Question question = null;
 		BusinessException businessExc = new BusinessException();
+		
 		try {
 			checkQuestion(quest, businessExc);
 			if(!businessExc.hasErreurs())	{
@@ -28,48 +30,51 @@ public class ReferentielManager {
 			}
 			
 		} catch (BusinessException e) {
-			e.printStackTrace();
+			throw e;
 		}
 		return question;
 	}
 	
 	
-	public int removeQuestion(int id) {
+	public int removeQuestion(int id) throws BusinessException{
+		
+		BusinessException businessExc = new BusinessException();
 		int retval = 0;
+		
 		try {
 			if(id != 0) {
 				retval = dao.removeQuestion(id);
-			}else {
-				BusinessException businessExc = new BusinessException();
-				// ajouter le code
+			}else {		
+				businessExc.ajouterErreur(CodesResultatBLL.OBJET_NULL);
 				throw businessExc;
 			}
 			
 		} catch (BusinessException e) {
-			e.printStackTrace();
+			throw e;
 		}
 		return retval;
 	}
 	
 	private  void checkQuestion(Question question, BusinessException businessExc) {
+		
 		if(question.getEnonce() == null || question.getEnonce().length() < 10) {
-			// ajouter le code
+			businessExc.ajouterErreur(CodesResultatBLL.REGLE_REFERENTIEL_ENONCE_ERREUR);
 		}
 		if(question.isUneReponse() == null) {
-			// ajouter le code
+			businessExc.ajouterErreur(CodesResultatBLL. REGLE_REFERENTIEL_UNEREPONSE_ERREUR);
 		}
 		if(question.getListeProp() == null) {
-			// ajouter le code
+			businessExc.ajouterErreur(CodesResultatBLL. REGLE_REFERENTIEL_LISTPROP_ERREUR);
 		}
 		if(question.getListeProp().size() < 4) {
-			// ajouter le code
+			businessExc.ajouterErreur(CodesResultatBLL.REGLE_REFERENTIEL_LISTPROP_LONG_ERREUR);
 		}
 		if(question.getPoints() == 0 || question.getPoints()<0 || question.getPoints()>10) {
-			// ajouter le code
+			businessExc.ajouterErreur(CodesResultatBLL. REGLE_REFERENTIEL_POINTS_ERREUR);
 		}
 		for(Proposition p : question.getListeProp()) {
 			if(p.getEnonce().length() == 0) {
-				// ajouter le code
+				businessExc.ajouterErreur(CodesResultatBLL.REGLE_REFERENTIEL_PROP_LONG_ERREUR);
 			}
 		}
 		
