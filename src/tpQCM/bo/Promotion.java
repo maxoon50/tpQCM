15f7116870f3e29fc7861fa8708ab6085d53d4e6
Unknown <ml.maroux@gmail.com>
2018-03-29 12:27:10 +0200

    update: bo promotion

--- src/tpQCM/bo/Promotion.java
+++ src/tpQCM/bo/Promotion.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Promotion {
 	
-	private int codePromo;
+	private String codePromo;
 	private String libelle;
 	
 	List<Utilisateur> stagiaires;
@@ -21,8 +21,8 @@ public class Promotion {
 	/*
 	 * constructeur libelle pour creation et insertion en BDD
 	 */
-	public Promotion(String libelle) {
-		super();
+	public Promotion(String codePromo, String libelle) {
+		this.codePromo = codePromo;
 		this.libelle = libelle;
 	}
 
@@ -40,11 +40,44 @@ public class Promotion {
 	/*
 	 * constructeur total
 	 */
-	public Promotion(int codePromo, String libelle, List<Utilisateur> stagiaires) {
+	public Promotion(String codePromo, String libelle, List<Utilisateur> stagiaires) {
 		super();
 		this.codePromo = codePromo;
 		this.libelle = libelle;
 		this.stagiaires = stagiaires;
 	}
+
+
+	public String getCodePromo() {
+		return codePromo;
+	}
+
+
+	public void setCodePromo(String codePromo) {
+		this.codePromo = codePromo;
+	}
+
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+
+	public List<Utilisateur> getStagiaires() {
+		return stagiaires;
+	}
+
+
+	public void setStagiaires(List<Utilisateur> stagiaires) {
+		this.stagiaires = stagiaires;
+	}
+	
+	
+	
 	
 }
